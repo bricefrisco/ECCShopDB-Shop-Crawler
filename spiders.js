d62af459470a9f897dac884531ba38b4b1ee828b
bricefrisco <bricemfrisco@gmail.com>
2021-01-08 19:09:48 -0600

    Update cron scheduler for GMT

--- spiders.js
+++ spiders.js
@@ -5,17 +5,17 @@ const cron = require('node-cron');
 
 console.log('Scheduling tasks.');
 
-cron.schedule('0 0,4,8,10,12,14,16,18,20,21 * * *', () => {
+cron.schedule('0 5,9,13,15,17,19,21,23,25,1 * * *', () => {
   console.log('Scheduler for updating chest shops has started.');
   shopSignsSpider.updateShopSigns();
 });
 
-cron.schedule('0 2 * * *', () => {
+cron.schedule('0 7 * * *', () => {
   console.log('Scheduler for updating regions has started.');
   regionsSpider.updateRegions();
 });
 
-cron.schedule('0 6 * * *', () => {
+cron.schedule('0 11 * * *', () => {
   console.log('Scheduler for updating players has started.');
   playersSpider.updatePlayers();
 });
